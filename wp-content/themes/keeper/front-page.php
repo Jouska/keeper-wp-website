a4ec437bc2c60a848ca0becbd364fd917e46339f
Jouska <tomhalliwell89@gmail.com>
2019-04-16 17:30:57 +0100

    Updated line spacing on front page again

--- wp-content/themes/keeper/front-page.php
+++ wp-content/themes/keeper/front-page.php
@@ -7,19 +7,11 @@
                     Web design solutions, image making services and brand messaging for your story.</h2>
                     </div>
                     <div class="about-us-strip__text-area">
-                        <p>We're here to make you look good. We started Keeper in mid-2018 because there were a lot of places you could go to buy design, websites and copy, but very few places that tied these things together with a compelling vision, or pragmatic strategies. 
-                            <br>
-                            <br>
-                            Even fewer agencies felt personally invested in their clients success. 
-                            <br>
-                            <br>
-                            It goes like this: de-mystify the problem, come up with a plan and execute it. 
-                            <br>
-                            <br>
-                            That's what we do. 
-                            <br>
-                            <br>
-                            So, if you're a small company who's good at what they do, but needs help  presenting their work to the world, or if you're a medium sized organisation that wants to upgrade their relationship with their customers and clients, we would love to hear from you.</p>
+                        <p>We're here to make you look good. We started Keeper in mid-2018 because there were a lot of places you could go to buy design, websites and copy, but very few places that tied these things together with a compelling vision, or pragmatic strategies.</p> 
+                        <p>Even fewer agencies felt personally invested in their clients success.</p>
+                        <p>It goes like this: de-mystify the problem, come up with a plan and execute it. </p>
+                        <p>That's what we do.</p>
+                        <p>So, if you're a small company who's good at what they do, but needs help  presenting their work to the world, or if you're a medium sized organisation that wants to upgrade their relationship with their customers and clients, we would love to hear from you.</p>
                     </div>
                     <div class="about-us-strip__contact-area">
                         <div class="about-us-strip__contact-area__text">
