cb974aa5dc8bc208b58cd3b6a1e78cb3c6095ef6
Jouska <tomhalliwell89@gmail.com>
2019-04-18 16:57:27 +0100

    Added new skills Feature to front page.

--- wp-content/themes/keeper/js/functions.js
+++ wp-content/themes/keeper/js/functions.js
@@ -2,3 +2,18 @@
     $('figure.wp-caption.aligncenter').removeAttr('style');
     $('img.aligncenter').wrap('<figure class="centered-image"/>');
 }) (jQuery);
+
+
+(function($) {
+
+    var skillsButton = $('#skills-button');
+    var aboutText = $('.about-us-strip__text-area');
+    var skillsText = $('.about-us-strip__skills-area');
+
+    skillsButton.click(function() {
+        aboutText.toggleClass('fade');
+        skillsText.toggleClass('unfade');
+    });
+
+}) (jQuery);
+
